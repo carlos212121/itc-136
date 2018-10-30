38b97666069bb018fbe4cc115adf257548a145f9
carlos <carygennessy@gmail.com>
2018-10-29 21:08:13 -0700

    I made a script

--- my_script.sh
+++ my_script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "hello ITC-136"
 echo "hello From github"
+
+echo "This is the current working directory:"
+pwd
+
+echo "This is the version of linux you're running"
+uname -a
+
+
+echo "These are your logged in users:"
+who
