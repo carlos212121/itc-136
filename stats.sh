1fc34baf96140993e3555841a76905cd5816b96c
carlos212121 <44388458+carlos212121@users.noreply.github.com>
2018-11-19 20:21:31 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -8,4 +8,17 @@ connections=$( netstat | grep tcp )
 
 users=$( who )
 
-echo "
+echo "This is a snapshot of your current system:
+
+       Disk Used: $diskused
+       Free Memory: $memfree
+       Logged in Users: $user
+       
+       
+ open internet connections: $connection"
+ 
+ echo "
+ colar info:
+ http://misk.flogisoft.com/bash/tip_colors_and_formatting
+ http://wiki.bash-hackers.org/scripting/style
+ https://help.ubuntu.com/community/UnityLauncherAndDesktopfiles
